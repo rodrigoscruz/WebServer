7d4f5b89c5b620d3b8c642680bf6fa7026bed322
Rodrick <46389582+rodrigoscruz@users.noreply.github.com>
2023-04-18 15:44:29 -0300

    Index

--- Enquete/Controllers/HomeController.cs
+++ Enquete/Controllers/HomeController.cs
@@ -11,12 +11,6 @@ namespace Enquete.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
         public IActionResult Index()
         {
@@ -28,10 +22,5 @@ namespace Enquete.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
